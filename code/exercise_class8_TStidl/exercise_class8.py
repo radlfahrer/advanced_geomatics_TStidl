a6719ac6a55ad63e74b35f9e721714f9559348e8
radlfahrer <timo@stidl.at>
2024-05-30 22:44:09 +0200

    .

--- code/exercise_class8_TStidl/exercise_class8.py
+++ code/exercise_class8_TStidl/exercise_class8.py
@@ -3,12 +3,36 @@ from pyqgis_scripting_ext.core import *
 
 
 # paths
-nat_earth_folder = 'C:/Users/timo/Documents/Master/2_Semester/Advanced_geomatics/natural_earth_vector.gpkg/packages/'
-geopackagePath = nat_earth_folder + 'natural_earth_vector.gpkg'
+
+data_folder = 'C:/Users/timo/Documents/GitHub/advanced_geomatics/data/'
+csvPath = data_folder + 'dpc-covid19-ita-regioni.csv'
+geopackagePath = data_folder + 'reduced_ne.gpkg'
+
 provincesName = 'ne_10m_admin_1_states_provinces'
 
-data_folder = 'C:/Users/timo/Documents/GitHub/advanced_geomatics/Exercises/exercise_class8_TStidl/'
-csvPath = data_folder + 'dpc-covid19-ita-regioni.csv'
+# gpkg combine provinces to regions
+
+countriesLayer = HVectorLayer.open(geopackagePath, provincesName)
+countriesLayer.subset_filter("admin = 'Italy'")
+#print(countriesLayer.fields.items())
+#nameIndexCountries = countriesLayer.field_index('admin')
+nameIndexRegions = countriesLayer.field_index('region')
+countriesFeatures = countriesLayer.features()
+
+regions_gpkg = []
+regions_gpkgMap = {}
+
+for feature in countriesFeatures:
+    geometry = feature.geometry
+    region = feature.attributes[nameIndexRegions]
+    if regions_gpkgMap.get(region):
+        regions_gpkgMap[region].append()
+    regions_gpkgMap[region] = []
+
+
+
+print(regions_gpkgMap)
+
 
 with open(csvPath, 'r') as file:
     lines = file.readlines()
@@ -17,37 +41,38 @@ regionIndex = None
 for i in range(len(line1Split)):
     if line1Split[i] == 'denominazione_regione':
         regionIndex = i
+        latIndex = i+1
+        lonIndex = i+2
 
-regions = []
+# regions = []
+regionsMap = {}
+
+crsHelper = HCrs()
+crsHelper.from_srid(4326)
+crsHelper.to_srid(3857)
+
+#canvas = HMapCanvas.new()
+
+#osm = HMap.get_osm_layer()
+#canvas.set_layers([osm])
 
 for line in lines[1:]:
     line = line.strip()
     lineSplit = line.split(',')
     if lineSplit[regionIndex] not in regions:
         regions.append(lineSplit[regionIndex])
+    lon = float(lineSplit[lonIndex])
+    lat = float(lineSplit[latIndex])
+    
+    #canvas.add_geometry(crsHelper.transform(HPoint(lon,lat)))
 
-regionsMap = {}
+#canvas.show()
+
+
 
 #print(regions)
 
-countriesLayer = HVectorLayer.open(geopackagePath, provincesName)
-countriesLayer.subset_filter("admin = 'Italy'")
-#print(countriesLayer.fields.items())
-nameIndexCountries = countriesLayer.field_index('admin')
-nameIndexRegions = countriesLayer.field_index('region')
-countriesFeatures = countriesLayer.features()
 
-#regions_gpkg = []
-regions_gpkgMap = {}
-
-for feature in countriesFeatures:
-    if feature.attributes[nameIndexRegions] not in regions_gpkg:
-        regions_gpkg.append(feature.attributes[nameIndexRegions])
-        regions_gpkgMap[region] = []
-
-
-
-#print(regions_gpkg)
 
 same_name = []
 different_name = []
@@ -57,8 +82,8 @@ for region in regions:
         same_name.append(region)
     else:
         different_name.append(region)
-print(same_name)
-print(different_name)
+#print(same_name)
+#print(different_name)
 
 conversionMap = {
     
--- code/nat_earth_reduction.py
+++ code/nat_earth_reduction.py
@@ -1,11 +1,24 @@
 # creating new gpkg from natural_earth_vector.gpkg with only used layers
 
-nat_earth_folder = 'C:/Users/timo/Documents/Master/2_Semester/Advanced_geomatics/natural_earth_vector.gpkg/packages/'
+from pyqgis_scripting_ext.core import *
+
+nat_earth_folder = 'C:/Users/timo/Documents/GitHub/advanced_geomatics/data/natural_earth_vector.gpkg/packages/'
 geopackagePath = nat_earth_folder + 'natural_earth_vector.gpkg'
+folder = 'C:/Users/timo/Documents/GitHub/advanced_geomatics/data/'
+layers = ['ne_10m_admin_1_states_provinces', 'ne_50m_admin_0_countries', 'ne_50m_populated_places', 'ne_10m_rivers_lake_centerlines_scale_rank']
 
 layer1 = 'ne_10m_admin_1_states_provinces'
 layer2 = 'ne_50m_admin_0_countries'
 layer3 = 'ne_50m_populated_places'
 layer4 = 'ne_10m_rivers_lake_centerlines_scale_rank'
 
-    
+path = folder + 'reduced_ne.gpkg'
+
+for i, layer in enumerate(layers):
+    ne_layer = HVectorLayer.open(geopackagePath, layer)
+    if i == 0:
+        error = ne_layer.dump_to_gpkg(path, overwrite=True)
+    else:
+        error = ne_layer.dump_to_gpkg(path, overwrite=False)
+    if error:
+        print(error)
