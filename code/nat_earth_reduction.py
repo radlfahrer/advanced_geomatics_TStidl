e534b28639c5364254e85d9cc76a83d32f532255
radlfahrer <timo@stidl.at>
2024-05-30 21:15:55 +0200

    re-structuring

---
+++ code/nat_earth_reduction.py
@@ -0,0 +1,11 @@
+# creating new gpkg from natural_earth_vector.gpkg with only used layers
+
+nat_earth_folder = 'C:/Users/timo/Documents/Master/2_Semester/Advanced_geomatics/natural_earth_vector.gpkg/packages/'
+geopackagePath = nat_earth_folder + 'natural_earth_vector.gpkg'
+
+layer1 = 'ne_10m_admin_1_states_provinces'
+layer2 = 'ne_50m_admin_0_countries'
+layer3 = 'ne_50m_populated_places'
+layer4 = 'ne_10m_rivers_lake_centerlines_scale_rank'
+
+    
